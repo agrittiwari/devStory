4f43c73e0ccfbbfc9d6b50ba7d64b50d0608f50a
agrittiwari <agrit1408@gmail.com>
2023-09-28 08:37:24 +0530

    A good exploration of app router
    Any route under app route is still coming under Tabs after limiting the layout to two routes for Tab Layout for android.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,10 +5,13 @@ import {
   ThemeProvider,
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { SplashScreen, Stack } from "expo-router";
+import { Link, SplashScreen, Stack, Tabs } from "expo-router";
 import { useEffect } from "react";
-import { useColorScheme } from "react-native";
+import { Platform, useColorScheme, Pressable } from "react-native";
 import { H1, H2 } from "../components/basic/StyledText";
+// import TabLayout from "./(2tabs)/_layout";
+import Colors from "../constants/Colors";
+import Icon from "../components/basic/Icon";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -17,7 +20,7 @@ export {
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(tabs)",
+  initialRouteName: "(2tabs)",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -43,8 +46,10 @@ export default function RootLayout() {
   if (!loaded) {
     return null;
   }
-
-  return <RootLayoutNav />;
+  if (Platform.OS === "web") {
+    return <RootLayoutNav />;
+  }
+  return <TabLayout />;
 }
 
 function RootLayoutNav() {
@@ -54,10 +59,51 @@ function RootLayoutNav() {
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <H1>This is a H1 tag</H1>
       <Stack>
-        <Stack.Screen name="2tabs" />
+        <Stack.Screen name="" />
         <Stack.Screen name="modal" options={{ presentation: "modal" }} />
       </Stack>
       <H2>This is a H2 tag</H2>
     </ThemeProvider>
+  );
+}
+
+function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors["light"].tint,
+      }}
+    >
+      <Tabs.Screen
+        name="one"
+        options={{
+          title: "Triggers",
+          tabBarIcon: ({ color }) => <Icon iconName="keyboard" color={color} />,
+          headerRight: () => (
+            <Link href="/modal" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <Icon
+                    iconName="account-circle"
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="two"
+        options={{
+          title: "React",
+          tabBarIcon: ({ color }) => (
+            <Icon iconName="video-label" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
